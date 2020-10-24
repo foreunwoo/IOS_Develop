39f14872303d934f0897c933f08c733a556d7449
foreunwoo <agasiya99@gmail.com>
2020-10-24 14:52:41 +0900

    BMI Calculator

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by eunwoo on 2020/10/22.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "Default"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 0.03140618443, green: 0.383912517, blue: 0.5748849511, alpha: 1)
+    }
+    
+    mutating func calculatorBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        }
+    }
+    
+}
