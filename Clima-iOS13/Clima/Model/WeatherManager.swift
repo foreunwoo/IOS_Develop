ffcaf2f89e7c45063df91dac6f6626eb6f537cb4
foreunwoo <agasiya99@gmail.com>
2020-10-27 20:16:20 +0900

    Networking

--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -9,10 +9,37 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherUrl = "api.openweathermap.org/data/2.5/find?apppid=0380287a176b477dac02999afa86e729&units=metric"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=0380287a176b477dac02999afa86e729&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        // 1. url 생성
+        if let url = URL(string: urlString) {
+            
+            // 2. url 세션 생성
+            let session = URLSession(configuration: .default)
+            
+            // 3. 태스크 생성
+            let task = session.dataTask(with: url) { (data, reponse, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
+            
+            // 4. 태스크 실행
+            task.resume()
+        }
+        
+    }
+    
 }
